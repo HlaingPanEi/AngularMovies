3a3568c16b6a47aef41321663832ee916d2bc984
Hlaing Pan Ei <hlaingpanei1515@gmail.com>
2024-11-27 13:02:04 +0630

    app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Results } from './results';
+import { MovieResponse } from './movie-response';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-movies';
+export class AppComponent implements OnInit {
+  
+  movieListResult?: Results[];
+  loading = true;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+      
+  }
+
+  getMovieList() {
+    return this.http.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=a99aca78f4fc7deb6897d69b78e298bf&language=en-US&page=1`);
+  }
+
+  getMovies(){
+    this.getMovieList()
+      .subscribe((response: MovieResponse) => {
+        this.movieListResult = response.results;
+        this.loading = false;
+      })
+  }
 }
+
